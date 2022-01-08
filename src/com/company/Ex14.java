b1b3a161487237798e76312dedf752f5f0308e06
Amir <amir.shafat1@gmail.com>
2022-01-08 22:13:02 +0200

    Initial commit

---
+++ src/com/company/Ex14.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/**
+ *
+ */
+public class Ex14 {
+
+    /**
+     *
+     * @param a
+     * @param x
+     * @param y
+     * @return
+     */
+    public static int findMinDiff (int[] a, int x, int y){
+        final int VALUE_MAX= a.length;
+
+    }
+
+}
