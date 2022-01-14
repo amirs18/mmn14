d87f383f9a11e5d65e227bbfa0867fe85a3036cc
amir <amir.shafat1@gmail.com>
2022-01-14 02:19:58 +0200

    need api

--- src/com/company/Ex14.java
+++ src/com/company/Ex14.java
@@ -6,29 +6,121 @@ package com.company;
 public class Ex14 {
 
     /**
+     * O(n),O(4)
      *
      * @param a
      * @param x
      * @param y
      * @return
      */
-    public static int findMinDiff (int[] a, int x, int y){
-        int minDistance=Integer.MAX_VALUE,tempX=-1,tempY=-1;
-        for (int i=0;i< a.length;i++){
-            if (a[i]==x){
-                tempX=i;
-                if (tempY>-1)
-                    if (minDistance>tempX-tempY)
-                    minDistance=tempX-tempY;
+    public static int findMinDiff(int[] a, int x, int y) {
+        int minDistance = Integer.MAX_VALUE, tempX = -1, tempY = -1;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == x) {
+                tempX = i;
+                if (tempY > -1)
+                    if (minDistance > tempX - tempY)
+                        minDistance = tempX - tempY;
             }
-            if (a[i]==y){
-                tempY=i;
-                if (tempX>-1)
-                    if (minDistance>tempY-tempX)
-                        minDistance=tempY-tempX;
+            if (a[i] == y) {
+                tempY = i;
+                if (tempX > -1)
+                    if (minDistance > tempY - tempX)
+                        minDistance = tempY - tempX;
             }
         }
-       return minDistance;
+        return minDistance;
+    }
+
+    /**
+     *
+     * @param mat
+     * @param num
+     * @return
+     */
+    public static boolean search(int[][] mat, int num) {
+        return search(mat,num, mat.length/2,0,0);
+    }
+
+    /**
+     *
+     * @param mat
+     * @param num
+     * @param length
+     * @param i
+     * @param j
+     * @return
+     */
+    public static boolean search(int[][] mat, int num, int length, int i, int j) {
+        if (!(mat[i][j]>num)) {
+            if (mat[i][j] == num) {
+                System.out.println();
+                System.out.println("row ="+i);
+                System.out.println("col ="+j);
+                return true;
+            }
+            if (length >= 1)
+                return search(mat, num, length / 2, i, j)
+                        || search(mat, num, length / 2, i + length, j)
+                        || search(mat, num, length / 2, i + length, j + length)
+                        || search(mat, num, length / 2, i, j + length);
+        }
+        return false;
+    }
+
+    /**
+     *
+     * @param arr
+     * @return
+     */
+    public static boolean equalSplit(int[] arr) {
+        if (arr.length % 2 != 0)
+            return false;
+
+        return equalSplit(arr,0,0,0,0,0);
+    }
+
+    /**
+     *
+     * @param arr
+     * @param i
+     * @param sum1
+     * @param sum2
+     * @param counter1
+     * @param counter2
+     * @return
+     */
+    public static boolean equalSplit(int[] arr,int i,int sum1,int sum2,int counter1,int counter2) {
+        if (i==arr.length)
+            return sum1==sum2&&counter1==counter2;
+        return equalSplit(arr,i+1,sum1+arr[i],sum2,counter1+1,counter2)
+                ||equalSplit(arr,i+1,sum1,sum2+arr[i],counter1,counter2+1);
+    }
+
+    /**
+     *
+     * @param n
+     * @return
+     */
+    public static boolean isSpecial (int n){
+        return isSpecial(n,2,n);
+    }
+
+    /**
+     *
+     * @param n
+     * @param delete
+     * @param place
+     * @return
+     */
+    public static boolean isSpecial (int n,int delete,int place){
+        if (place%delete==0)
+            return false;
+        if (place<delete) {
+            System.out.println(place);
+            return true;
+        }
+        return isSpecial(n,delete+1,place-place/delete);
     }
 
 }
--- src/com/company/Ex14Test.java
+++ src/com/company/Ex14Test.java
@@ -11,15 +11,14 @@ package com.company;
 
 public class Ex14Test extends Ex14
 {
-    public static void main(String args[])
-    {
-        int[] arr = {1, 3, 5, 4, 8, 2, 4, 3, 6, 5};
+    public static void main(String args[]) {
+     /*   int[] arr = {1, 3, 5, 4, 8, 2, 4, 3, 6, 5};
 
         int num, num2;
 
         System.out.println("***** Qst 1 - diffMinNum *****");
         System.out.print("Array: ");
-        for (int i=0; i<arr.length; i++)
+        for (int i = 0; i < arr.length; i++)
             System.out.print(arr[i] + " ");
 
         num = findMinDiff(arr, 1, 6);
@@ -28,7 +27,7 @@ public class Ex14Test extends Ex14
         if (num == num2 && num == 8)
             System.out.println("OK");
         else
-            System.out.println("Error - your distance is: " + num +" or " + num2 + " Expect: 8");
+            System.out.println("Error - your distance is: " + num + " or " + num2 + " Expect: 8");
 
         num = findMinDiff(arr, 3, 4);
         num2 = findMinDiff(arr, 4, 3);
@@ -36,7 +35,7 @@ public class Ex14Test extends Ex14
         if (num == num2 && num == 1)
             System.out.println("OK");
         else
-            System.out.println("Error - your distance is: " + num +" or " + num2 + " Expect: 1");
+            System.out.println("Error - your distance is: " + num + " or " + num2 + " Expect: 1");
 
         num = findMinDiff(arr, 8, 6);
         num2 = findMinDiff(arr, 6, 8);
@@ -44,7 +43,7 @@ public class Ex14Test extends Ex14
         if (num == num2 && num == 4)
             System.out.println("OK");
         else
-            System.out.println("Error - your distance is: " + num +" or " + num2 + " Expect: 4");
+            System.out.println("Error - your distance is: " + num + " or " + num2 + " Expect: 4");
 
         num = findMinDiff(arr, 7, 8);
         num2 = findMinDiff(arr, 8, 7);
@@ -52,7 +51,7 @@ public class Ex14Test extends Ex14
         if (num == num2 && num == 2147483647)
             System.out.println("OK");
         else
-            System.out.println("Error - your distance is: " + num +" or " + num2 + " Expect: 2147483647");
+            System.out.println("Error - your distance is: " + num + " or " + num2 + " Expect: 2147483647");
 
         num = findMinDiff(arr, 9, 7);
         num2 = findMinDiff(arr, 7, 9);
@@ -60,7 +59,7 @@ public class Ex14Test extends Ex14
         if (num == num2 && num == 2147483647)
             System.out.println("OK");
         else
-            System.out.println("Error - your distance is: " + num +" or " + num2 + " Expect: 2147483647");
+            System.out.println("Error - your distance is: " + num + " or " + num2 + " Expect: 2147483647");
 
         num = findMinDiff(arr, 5, 6);
         num2 = findMinDiff(arr, 6, 5);
@@ -68,7 +67,7 @@ public class Ex14Test extends Ex14
         if (num == num2 && num == 1)
             System.out.println("OK");
         else
-            System.out.println("Error - your distance is: " + num +" or " + num2 + " Expect: 1");
+            System.out.println("Error - your distance is: " + num + " or " + num2 + " Expect: 1");
 
         num = findMinDiff(arr, 1, 3);
         num2 = findMinDiff(arr, 3, 1);
@@ -76,7 +75,7 @@ public class Ex14Test extends Ex14
         if (num == num2 && num == 1)
             System.out.println("OK");
         else
-            System.out.println("Error - your distance is: " + num +" or " + num2 + " Expect: 1");
+            System.out.println("Error - your distance is: " + num + " or " + num2 + " Expect: 1");
 
         num = findMinDiff(arr, 3, 5);
         num2 = findMinDiff(arr, 5, 3);
@@ -84,19 +83,18 @@ public class Ex14Test extends Ex14
         if (num == num2 && num == 1)
             System.out.println("OK");
         else
-            System.out.println("Error - your distance is: " + num +" or " + num2 + " Expect: 1");}}
+            System.out.println("Error - your distance is: " + num + " or " + num2 + " Expect: 1");
 
 
-/**
         System.out.println("\n***** Qst 2 - search - 2x2 array *****");
-        int[][] arr2x2 ={{1, 2},
+        int[][] arr2x2 = {{1, 2},
                 {4, 3}};
 
         System.out.print("Test #1:......");
-        if (search(arr2x2, 1))
+        if (search(arr2x2, 5))
             System.out.println("OK");
         else
-            System.out.println("Error - your search: " + search(arr2x2, 1) + "Expect: true");
+            System.out.println("Error - your search: " + search(arr2x2, 5) + "Expect: true");
 
         System.out.print("Test #2:......");
         if (search(arr2x2, 2))
@@ -129,12 +127,11 @@ public class Ex14Test extends Ex14
             System.out.println("Error - your search: " + search(arr2x2, 0) + "Expect: false");
 
 
-
         System.out.println("\n***** Qst 2 - search - 4x4 array *****");
-        int[][] arr4x4 ={{1, 2, 5, 6},
+        int[][] arr4x4 = {{1, 2, 5, 6},
                 {4, 3, 8, 7},
-                {13,14,9, 10},
-                {16,15,12,11}};
+                {13, 14, 9, 10},
+                {16, 15, 12, 11}};
 
         System.out.print("Test #1:......");
         if (search(arr4x4, 1))
@@ -197,16 +194,15 @@ public class Ex14Test extends Ex14
             System.out.println("Error - your search: " + search(arr4x4, 0) + "Expect: false");
 
 
-
         System.out.println("\n***** Qst 2 - search - 8x8 array *****");
-        int[][] arr8x8 ={{1,2,5,6, 17, 18, 21, 22},
-                {4,3,8,7, 20, 19, 24, 23},
-                {13,14,9,10, 29,30,25,26},
-                {16,15,12,11,32,31,28,27},
-                {49,50,53,54,33,34,37,38},
-                {52,51,56,55,36,35,40,39},
-                {61,62,57,58,45,46,41,42},
-                {64,63,60,59,48,47,44,43}};
+        int[][] arr8x8 = {{1, 2, 5, 6, 17, 18, 21, 22},
+                {4, 3, 8, 7, 20, 19, 24, 23},
+                {13, 14, 9, 10, 29, 30, 25, 26},
+                {16, 15, 12, 11, 32, 31, 28, 27},
+                {49, 50, 53, 54, 33, 34, 37, 38},
+                {52, 51, 56, 55, 36, 35, 40, 39},
+                {61, 62, 57, 58, 45, 46, 41, 42},
+                {64, 63, 60, 59, 48, 47, 44, 43}};
 
         System.out.print("Test #1:.......");
         if (search(arr8x8, 1))
@@ -324,25 +320,25 @@ public class Ex14Test extends Ex14
 
 
         System.out.println("\n***** Qst 2 - search - 16x16 array *****");
-        int[][] arr16x16 = {{1,  2,   5,  6,    17, 18,  21, 22,   65, 66,  69, 70,   81, 82,  85, 86},
-                {4,  3,   8,  7,    20, 19,  24, 23,   68, 67,  72, 71,   84, 83,  88, 87},
-                {13, 14,  9,  10,   29, 30,  25, 26,   77, 78,  73, 74,   93, 94,  89, 90},
-                {16, 15,  12, 11,   32, 31,  28, 27,   80, 79,  76, 75,   96, 95,  92, 91},
+        int[][] arr16x16 = {{1, 2, 5, 6, 17, 18, 21, 22, 65, 66, 69, 70, 81, 82, 85, 86},
+                {4, 3, 8, 7, 20, 19, 24, 23, 68, 67, 72, 71, 84, 83, 88, 87},
+                {13, 14, 9, 10, 29, 30, 25, 26, 77, 78, 73, 74, 93, 94, 89, 90},
+                {16, 15, 12, 11, 32, 31, 28, 27, 80, 79, 76, 75, 96, 95, 92, 91},
 
-                {49, 50,  53, 54,   33, 34,  37, 38,   113, 114,  117, 118,   97,  98,   101, 102},
-                {52, 51,  56, 55,   36, 35,  40, 39,   116, 115,  120, 119,   100, 99,   104, 103},
-                {61, 62,  57, 58,   45, 46,  41, 42,   125, 126,  121, 122,   109, 110,  105, 106},
-                {64, 63,  60, 59,   48, 47,  44, 43,   128, 127,  124, 123,   112, 111,  108, 107},
+                {49, 50, 53, 54, 33, 34, 37, 38, 113, 114, 117, 118, 97, 98, 101, 102},
+                {52, 51, 56, 55, 36, 35, 40, 39, 116, 115, 120, 119, 100, 99, 104, 103},
+                {61, 62, 57, 58, 45, 46, 41, 42, 125, 126, 121, 122, 109, 110, 105, 106},
+                {64, 63, 60, 59, 48, 47, 44, 43, 128, 127, 124, 123, 112, 111, 108, 107},
 
-                {193, 194,  197, 198,   209, 210,  213, 214,   129, 130,  133, 134,   145, 146,  149, 150},
-                {196, 195,  200, 199,   212, 211,  216, 215,   132, 131,  136, 135,   148, 147,  152, 151},
-                {205, 206,  201, 202,   221, 222,  217, 218,   141, 142,  137, 138,   157, 158,  153, 154},
-                {208, 207,  204, 203,   224, 223,  220, 219,   144, 143,  140, 139,   160, 159,  156, 155},
+                {193, 194, 197, 198, 209, 210, 213, 214, 129, 130, 133, 134, 145, 146, 149, 150},
+                {196, 195, 200, 199, 212, 211, 216, 215, 132, 131, 136, 135, 148, 147, 152, 151},
+                {205, 206, 201, 202, 221, 222, 217, 218, 141, 142, 137, 138, 157, 158, 153, 154},
+                {208, 207, 204, 203, 224, 223, 220, 219, 144, 143, 140, 139, 160, 159, 156, 155},
 
-                {241, 242,  245, 246,   225, 226,  229, 230,   177, 178,  181, 182,   161, 162,  165, 166},
-                {244, 243,  248, 247,   228, 227,  232, 231,   180, 179,  184, 183,   164, 163,  168, 167},
-                {253, 254,  249, 250,   237, 238,  233, 234,   189, 190,  185, 186,   173, 174,  169, 170},
-                {256, 255,  252, 251,   240, 239,  236, 235,   192, 191,  188, 187,   176, 175,  172, 171}};
+                {241, 242, 245, 246, 225, 226, 229, 230, 177, 178, 181, 182, 161, 162, 165, 166},
+                {244, 243, 248, 247, 228, 227, 232, 231, 180, 179, 184, 183, 164, 163, 168, 167},
+                {253, 254, 249, 250, 237, 238, 233, 234, 189, 190, 185, 186, 173, 174, 169, 170},
+                {256, 255, 252, 251, 240, 239, 236, 235, 192, 191, 188, 187, 176, 175, 172, 171}};
 
 
         System.out.print("Test #1:.......");
@@ -476,6 +472,9 @@ public class Ex14Test extends Ex14
             System.out.println("OK");
         else
             System.out.println("Error - your search: " + search(arr16x16, 257) + "Expect: false");
+    }}
+
+
 
 
 
@@ -484,7 +483,7 @@ public class Ex14Test extends Ex14
 
         System.out.println("\n***** Qst 3 - equalSplit *****");
 
-        arr = new int[]{-3, 5, 12, 14, -9, 13};
+        int[] arr = new int[]{-3, 5, 12, 14, -9, 13};
         System.out.print("Test #1:......");
         if (equalSplit(arr))
             System.out.println("OK");
@@ -575,6 +574,8 @@ public class Ex14Test extends Ex14
         else
             System.out.println("Error - your equalSplit: " + equalSplit(arr) + "Expect: true");
 
+      */
+
 
 
 
@@ -582,10 +583,10 @@ public class Ex14Test extends Ex14
 
         System.out.println("\n***** Qst 4 - isSpecial *****");
         System.out.print("Test #1 - 1:......");
-        if (isSpecial(1))
+        if (isSpecial(63))
             System.out.println("OK");
         else
-            System.out.println("Error - your isSpecial: " + isSpecial(1) + "Expect: true");
+            System.out.println("Error - your isSpecial: " + isSpecial(58) + "Expect: true");
 
         System.out.print("Test #2 - 2:......");
         if (!isSpecial(2))
@@ -640,6 +641,7 @@ public class Ex14Test extends Ex14
             System.out.println("OK");
         else
             System.out.println("Error - your equalSplit: " + isSpecial(27) + "Expect: false");
+
+
     }//End of Main
 }//End of Test
-**/
